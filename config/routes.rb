b35e54c10b0259a258daeded5466fd5cc56ed6dd
Jess15 <jesicaespejo.15@gmail.com>
2017-03-30 15:32:37 -0300

    project

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  #devise_for :users
+  post 'auth_user' => 'authentication#authenticate_user'
+  get 'home' => 'home#index'
+  resources :todos do
+    resources :items
+  end
+end
